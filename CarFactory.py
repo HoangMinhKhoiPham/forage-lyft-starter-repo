246e19fe4e1892b556af56a97a74564154cc1906
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-05-02 23:04:00 -0400

    Final update for task 2

--- CarFactory.py
+++ CarFactory.py
@@ -1,4 +1,38 @@
 from abc import ABC
 from car import Car
 from engine.capulet_engine import CapuletEngine
-from Battery.Battery import Battery
+from Battery.SplindlerBattery import SplindlerBattery
+from engine.willoughby_engine import WilloughbyEngine
+from Battery.NubbinBattery import NubbinBattery
+from engine.sternman_engine import SternmanEngine
+
+
+class CarFactory(Car, ABC):
+    def __int__(self) -> None:
+        pass
+
+    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage)->Car:
+        engine = CapuletEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage, )
+        battery = SplindlerBattery(current_date=current_date,last_service_date=last_service_date)
+        return Car(engine, battery)
+
+    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = WilloughbyEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+        battery = SplindlerBattery(last_service_date=last_service_date, current_date=current_date)
+        return Car(engine, battery)
+
+    def create_palindrome(self, current_date, last_service_date, warning_light_is_on) -> Car:
+        engine = SternmanEngine(warning_light_is_on=warning_light_is_on)
+        battery = SplindlerBattery(last_service_date=last_service_date, current_date=current_date)
+        return Car(engine, battery)
+
+    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = WilloughbyEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+        battery = NubbinBattery(last_service_date=last_service_date, current_date=current_date)
+        return Car(engine, battery)
+
+    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = CapuletEngine(current_mileage=current_mileage, last_service_mileage=last_service_mileage)
+        battery = NubbinBattery(last_service_date=last_service_date, current_date=current_date)
+        return Car(engine, battery)
+
--- car.py
+++ car.py
@@ -1,10 +1,14 @@
+from abc import abstractmethod
 from Serviceable import Serviceable
-from Battery import Battery
-class Car(Serviceable, Battery):
+from Battery.Battery import Battery
+from engine.engine import Engine
+
+
+class Car(Serviceable):
     def __init__(self, engine: Engine, battery: Battery):
         self.engine = engine
         self.battery = battery
 
     @abstractmethod
     def needs_service(self):
-        pass
+        return self._engine.need_service() or self._battery.need_service()
