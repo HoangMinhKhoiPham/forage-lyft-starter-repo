0fe76e7a35c2fe746891ab1bc582e3706c9179d4
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-05-02 23:09:56 -0400

    Updated final

--- Battery/SplindlerBattery.py
+++ Battery/SplindlerBattery.py
@@ -1,5 +1,5 @@
 from Battery import Battery
-from datetime import datetime
+from utils import add_years_to_date
 
 
 class SplindlerBattery(Battery):
@@ -8,9 +8,8 @@ class SplindlerBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
             return False
-
